c07093da868c66148286b8f0282fb027926ae47f
johnloydVramos <55616510+johnloydVramos@users.noreply.github.com>
2022-02-18 10:53:28 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -6,5 +6,4 @@ DEFINE('DB_PASSWORD', '');
 DEFINE('DB_NAME', 'online_scheduling');
 
  $con = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) or die ("Could not connect to Database");
- $dbc = $con;
 ?>
